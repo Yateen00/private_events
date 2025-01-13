48385f687504012290012a0c577c316d9305c83b
Yateen00 <codingdudelolhow@gmail.com>
2025-01-13 22:25:38 +0530

    modified _show to include editing links if viewd from my_events page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   resources :events
+  resources :users, only: [ :show ]
   root "events#index"
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
