1f0b5654375ceab9682d476a4692485204fe5529
Yateen00 <codingdudelolhow@gmail.com>
2025-01-13 21:27:53 +0530

    changed column in db to be caled creator. and changed index and _event of events view

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_13_145017) do
     t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
-    t.index ["user_id"], name: "index_events_on_user_id"
+    t.integer "creator_id", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,5 +35,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_13_145017) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "events", "users"
+  add_foreign_key "events", "users", column: "creator_id"
 end
