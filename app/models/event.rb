5fe93877d4a89f989dfad987b705faeb254bec91
Yateen00 <codingdudelolhow@gmail.com>
2025-01-13 21:04:47 +0530

    added connection between user and events through creator. havent made changes tio views yet

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,3 @@
 class Event < ApplicationRecord
+  belongs_to :creator, class_name: "User" , inverse_of: :created_events
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_13_133910) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_13_145017) do
   create_table "events", force: :cascade do |t|
     t.datetime "date", null: false
     t.string "location", null: false
@@ -18,6 +18,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_13_133910) do
     t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +34,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_13_133910) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "events", "users"
 end
