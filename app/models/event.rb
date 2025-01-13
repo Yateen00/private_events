c7f648d7faca9fe612cf1c6730f0d3e7f135d5a4
Yateen00 <codingdudelolhow@gmail.com>
2025-01-14 00:57:39 +0530

    added valdiations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,23 @@
 class Event < ApplicationRecord
-  belongs_to :creator, class_name: "User" , inverse_of: :created_events
+  belongs_to :creator, class_name: "User", inverse_of: :created_events
+
+  validates :creator, :title, :description, :location, :date, presence: true
+  validate :date_cannot_be_in_the_past, on: :create
+
+  before_destroy :prevent_deletion_after_end_date
+
+  private
+
+  def date_cannot_be_in_the_past
+    if date.present? && date < DateTime.now
+      errors.add(:date, "Date can't be in the past")
+    end
+  end
+
+  def prevent_deletion_after_end_date
+    if date.present? && date < DateTime.now
+      errors.add(:base, "Cannot delete an event after its end date")
+      throw(:abort)
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :created_events, foreign_key: :creator_id, class_name: "Event", inverse_of: :creator
+  validates :name, presence: true
 end
