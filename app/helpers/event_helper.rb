aa640f753f8073d9cc982a93bc02c65417e08dcf
Yateen00 <codingdudelolhow@gmail.com>
2025-01-16 01:24:47 +0530

    remvoed inline styles

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -1,32 +1,32 @@
 module EventHelper
   def event_action_buttons(event, show_admin_links)
-    if show_admin_links && current_user == event.creator
-      edit_and_delete_buttons(event)
-    elsif current_user != event.creator
-      if user_signed_in? && event.attendees.include?(current_user)
-        cancel_registration_button(event)
-      else
-        register_button(event)
-      end
+    return button_tag "Event has ended", type: "button", class: "button button-ended" if event.date <= Time.now
+    return edit_and_delete_buttons(event) if show_admin_links && current_user == event.creator
+
+    if user_signed_in? && event.attendees.include?(current_user)
+      cancel_registration_button(event)
+    else
+      register_button(event)
     end
   end
 
   private
 
   def edit_and_delete_buttons(event)
-    button_to("Edit", edit_event_path(event), method: :get,
-                                              style: "display: inline-block; padding: 10px 20px; border-radius: 50px; background-color: #007bff; color: white; text-decoration: none; text-align: center; margin-right: 10px;") +
+    button_to("Edit", edit_event_path(event), method: :get, class: "button button-edit") +
       button_to("Delete", event_path(event), method: :delete, data: { confirm: "Are you sure?" },
-                                             style: "display: inline-block; padding: 10px 20px; border-radius: 50px; background-color: #ff0000; color: white; text-decoration: none; text-align: center;")
+                                             class: "button button-delete")
   end
 
   def cancel_registration_button(event)
-    button_to("Cancel Registration",
-              event_event_registration_path(event, event.event_registrations.find_by(attendee_id: current_user.id)), method: :delete, style: "display: inline-block; padding: 10px 20px; border-radius: 50px; background-color: #ff0000; color: white; text-decoration: none; text-align: center;")
+    registration = event.event_registrations.find_by(attendee_id: current_user.id)
+    return unless registration
+
+    button_to("Cancel Registration", event_event_registration_path(event, registration), method: :delete,
+                                                                                         class: "button button-delete")
   end
 
   def register_button(event)
-    button_to("Register", event_event_registrations_path(event), method: :post,
-                                                                 style: "display: inline-block; padding: 10px 20px; border-radius: 50px; background-color: #007bff; color: white; text-decoration: none; text-align: center;")
+    button_to("Register", event_event_registrations_path(event), method: :post, class: "button button-register")
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,26 +1,28 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", inverse_of: :created_events
 
-  has_many :event_registrations, class_name: "EventRegistration", foreign_key: "attended_event_id", inverse_of: :attended_event
+  has_many :event_registrations, class_name: "EventRegistration", foreign_key: "attended_event_id",
+                                 inverse_of: :attended_event
   has_many :attendees, through: :event_registrations, source: :attendee, inverse_of: :attended_events
 
   validates :creator, :title, :description, :location, :date, presence: true
   validate :date_cannot_be_in_the_past, on: :create
 
-  before_destroy :prevent_deletion_after_end_date
+  before_update :prevent_edit_after_end_date
+  before_destroy :prevent_edit_after_end_date
 
   private
 
   def date_cannot_be_in_the_past
-    if date.present? && date < DateTime.now
-      errors.add(:date, "Date can't be in the past")
-    end
+    return unless date.present? && date < DateTime.now
+
+    errors.add(:date, "Date can't be in the past")
   end
 
-  def prevent_deletion_after_end_date
-    if date.present? && date < DateTime.now
-      errors.add(:base, "Cannot delete an event after its end date")
-      throw(:abort)
-    end
+  def prevent_edit_after_end_date
+    return unless date.present? && date < DateTime.now
+
+    errors.add(:base, "Cannot edit an event after its end date")
+    throw(:abort)
   end
 end
