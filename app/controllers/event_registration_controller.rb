4d2280658319f6709fe5fea31308eb799ccb549e
Yateen00 <codingdudelolhow@gmail.com>
2025-01-15 20:36:07 +0530

    added registration controller and routes

---
+++ app/controllers/event_registration_controller.rb
@@ -0,0 +1,26 @@
+class EventRegistrationController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_event
+
+  def create
+    @event_registration = @event.event_registrations.build(attendee: current_user)
+    if @event_registration.save
+      @event_registration.registered!
+      redirect_to @event, notice: "You have successfully registered for this event"
+    else
+      redirect_to @event, alert: "You were unable to register for this event", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event_registration = @event.event_registrations.find_by(attendee: current_user)
+    @event_registration.destroy
+    redirect_to @event, notice: "You have successfully unregistered from this event"
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
+end
